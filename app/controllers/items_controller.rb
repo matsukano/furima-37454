090fe8f1bad80586306c245819176d413e734f2e
matsukano <matsuduck@gmail.com>
2022-02-28 20:38:29 +0900

    商品情報編集画面作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :redirect_root, only: :edit
+  before_action :redirect_new_user_session, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,8 +23,37 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :shipping_charge_id, :prefecture_id,  :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
-end
+
+  def redirect_root
+    item = Item.find(params[:id])
+    if user_signed_in?
+      unless current_user.id == item.user_id
+        redirect_to new_user_session_path
+      end
+    end
+  end
+
+  def redirect_new_user_session
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+end  
