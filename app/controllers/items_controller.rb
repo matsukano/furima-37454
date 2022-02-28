4739ad6047f595a76cf202c540bb1265ad070608
matsukano <matsuduck@gmail.com>
2022-02-28 21:42:48 +0900

    レビュー指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :redirect_root, only: :edit
-  before_action :redirect_new_user_session, only: :edit
-
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -21,15 +20,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -44,16 +40,12 @@ class ItemsController < ApplicationController
 
   def redirect_root
     item = Item.find(params[:id])
-    if user_signed_in?
-      unless current_user.id == item.user_id
-        redirect_to new_user_session_path
-      end
+    unless current_user.id == item.user_id
+      redirect_to root_path
     end
   end
 
-  def redirect_new_user_session
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end  
